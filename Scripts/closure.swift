f9753b3781d8b6d2bf427c42b4c7f7193c1b9aa3
Michal Cichon <michal.cichon@aol.com>
2016-03-03 23:13:30 +0100

    closure

---
+++ Scripts/closure.swift
@@ -0,0 +1,19 @@
+let numbers = [1, 2, 3, 4, 5, 6]
+
+let times3 = numbers.map({
+  (number: Int) -> Int in
+  let result = 3 * number
+  return result
+})
+
+let times3onlyEven = numbers.map({
+  (number: Int) -> Int in
+  if number % 2 == 0 {
+    return 3 * number
+  } else {
+    return 0;
+  }
+})
+
+print(times3)
+print(times3onlyEven)
